cef01e2aed87c07305e1083918445e43ee0cbe7a
Isfar Oshir <isfaroshir@gmail.com>
2025-01-25 13:43:39 -0500

    socketio integration with apollo-graphql

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,52 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+const { ApolloServer } = require("apollo-server-express");
 import { typeDefs } from "./schema.js";
 import { resolvers } from "./resolvers.js";
 import logger from "./utils/logger.js";
+import express from "express";
+import { Server as SocketIOServer } from "socket.io"; // Correct import
+import cors from "cors";
 
-async function startServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
+const app = express();
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+const corsOptions = {
+  origin: "*", // Adjust this to allow only specific origins
+  methods: "GET, POST",
+  credentials: false, // if you need to include cookies or authentication tokens
+};
+
+app.use(cors(corsOptions)); // Apply CORS middleware
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const http = app.listen(3001, () =>
+    logger.info("🚀 Server ready at http://localhost:3001" + server.graphqlPath)
+  );
+  const io = new SocketIOServer(http, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"], // Allowed methods for the WebSocket connection
+      allowedHeaders: ["Content-Type"], // Optional: Add allowed headers
+      credentials: true, // If you need to support cookies or authentication tokens
+    },
+  }); // Initialize socket.io server
+  io.on("connection", (socket) => {
+    logger.info("New socket connection established");
+
+    // Emit a message to the client immediately after connection
+    socket.emit("welcome", "Hello from the server!");
+
+    // Listen for a custom event
+    socket.on("clientMessage", (message) => {
+      logger.info("Received message from client:", message);
+      // Respond to the client with an acknowledgment
+      socket.emit("serverMessage", "Message received by server!");
+    });
   });
-  logger.info(`Server ready at ${url}`);
-}
+};
 
 startServer();
