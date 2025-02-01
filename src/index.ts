ac993766e3b8e80cbc18d8b6ae33c5bc6cf1fb23
Isfar Oshir <isfaroshir@gmail.com>
2025-02-01 00:43:40 -0500

    schema import fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { ApolloServer } from "apollo-server-express";
-import { typeDefs } from "./schema.js";
+import { typeDefs } from "./typedefs/schema.js";
 import { resolvers } from "./resolvers/resolvers.js";
 import logger from "./utils/logger.js";
 import express, { Application } from "express";
